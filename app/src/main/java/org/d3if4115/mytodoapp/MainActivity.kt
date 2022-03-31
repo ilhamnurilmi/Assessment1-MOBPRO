cef3188e468ab26da561cfecc408a76c3a58124f
ilhamnurilmi <ilhamnurilmi@student.telkomuniversity.ac.id>
2022-03-31 15:52:00 +0700

    Menambahkan Adapter

--- app/src/main/java/org/d3if4115/mytodoapp/MainActivity.kt
+++ app/src/main/java/org/d3if4115/mytodoapp/MainActivity.kt
@@ -7,6 +7,7 @@ import org.d3if4115.mytodoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var todoAdapter: TodoAdapter
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +15,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        binding.rvTodoItems.adapter = todoAdapter
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAddTodo.setOnClickListener {
+            val todoTitle = binding.etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                binding.etTodoTitle.text.clear()
+            }
+        }
+        binding.btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
