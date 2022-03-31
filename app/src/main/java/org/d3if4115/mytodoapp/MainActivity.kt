c01cfbc3f0875561f92c1aedd2bd04ac64467dea
ilhamnurilmi <ilhamnurilmi@student.telkomuniversity.ac.id>
2022-03-31 15:49:06 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if4115/mytodoapp/MainActivity.kt
+++ app/src/main/java/org/d3if4115/mytodoapp/MainActivity.kt
@@ -2,10 +2,17 @@ package org.d3if4115.mytodoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.d3if4115.mytodoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
